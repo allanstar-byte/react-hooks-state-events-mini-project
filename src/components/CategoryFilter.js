f713624306013ae7f0390ac147a977498b9d4884
allanstar-byte <allanmutisya263@gmail.com>
2022-07-22 10:16:00 +0300

    Task

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CategoryFilter() {
-  return (
-    <div className="categories">
-      <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
-    </div>
-  );
-}
+function CategoryFilter({ categories, onCategoryFilter }) {
+    const [selected, setSelected] = useState(0);
 
-export default CategoryFilter;
+    const buttonCategory = categories.map((category, index) =>
+        <
+        button key = { category }
+        className = {
+            selected === index ? "selected" : ""
+        }
+        onClick = {
+            () => {
+                onCategoryFilter(category)
+                setSelected(index)
+            }
+        } > { category } < /button>)
+
+        return ( <
+            div className = "categories" >
+            <
+            h5 > Category filters < /h5> { buttonCategory } <
+            /div>
+        );
+    }
+
+    export default CategoryFilter;
--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,21 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 
-function NewTaskForm() {
-  return (
-    <form className="new-task-form">
-      <label>
-        Details
-        <input type="text" name="text" />
-      </label>
-      <label>
-        Category
-        <select name="category">
-          {/* render <option> elements for each category here */}
-        </select>
-      </label>
-      <input type="submit" value="Add task" />
-    </form>
-  );
+function NewTaskForm({ onTaskFormSubmit, categories }) {
+    const [text, setText] = useState("");
+    const [category, setCategory] = useState("Code");
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        onTaskFormSubmit({ text, category })
+    }
+
+    function handleChange(event) {
+        setText(event.target.value)
+    }
+
+    function handleCategoryChange(event) {
+        setCategory(event.target.value)
+    }
+
+    const newCategories = categories.slice(1);
+
+    return ( <
+        form className = "new-task-form"
+        onSubmit = { handleSubmit } >
+        <
+        label >
+        Details <
+        input onChange = { handleChange }
+        value = { text }
+        type = "text"
+        name = "text" / >
+        <
+        /label> <
+        label >
+        Category <
+        select value = { category }
+        onChange = { handleCategoryChange }
+        name = "category" > {
+            newCategories.map((category, index) => {
+                return <option key = { index } > { category } < /option>
+            })
+        } <
+        /select> < /
+        label > <
+        input type = "submit"
+        value = "Add task" / >
+        <
+        /form>
+    );
 }
 
 export default NewTaskForm;
